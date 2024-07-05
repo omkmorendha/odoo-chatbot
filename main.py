70a45927d5b5e424f343bd7764831a1c606c4d6c
Om Morendha <omkmorendha@gmail.com>
2024-07-05 16:09:10 +0530

    Make automated query using saved LlamaIndex

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from dotenv import load_dotenv
+import psycopg2
 import os
+from dotenv import load_dotenv
+import openai
+from llama_index.core import VectorStoreIndex, get_response_synthesizer, load_index_from_storage, StorageContext
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
+from langchain_community.utilities import SQLDatabase
 
 load_dotenv()
 
@@ -8,5 +14,158 @@ DATABASE = {
     'USER': os.environ.get("DB_USER"),
     'PASSWORD': os.environ.get("DB_PASSWORD"),
     'HOST': os.environ.get("DB_HOST"),
-    'PORT': os.environ.get("DB_PORT"),
+    'PORT': int(os.environ.get("DB_PORT")),
 }
+
+
+def perform_sql_query(sql_query):
+    try:
+        conn = psycopg2.connect(
+            dbname=DATABASE['NAME'],
+            user=DATABASE['USER'],
+            password=DATABASE['PASSWORD'],
+            host=DATABASE['HOST'],
+            port=DATABASE['PORT']
+        )
+        
+        cursor = conn.cursor()
+        cursor.execute(sql_query)
+        
+        result = cursor.fetchall()
+        
+        conn.commit()
+        
+        cursor.close()
+        conn.close()
+        
+        return result, True
+    
+    except Exception as e:
+        print(f"Error executing SQL query: {e}")
+        return None, False
+
+
+def create_sql_query(query):
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context)
+
+    retriever = VectorIndexRetriever(
+        index=index,
+        similarity_top_k=2,
+    )
+
+    response_synthesizer = get_response_synthesizer(
+        response_mode="tree_summarize",
+    )
+
+    query_engine = RetrieverQueryEngine(
+        retriever=retriever,
+        response_synthesizer=response_synthesizer,
+    )
+
+    response = query_engine.query(f"Generate a strict POSTGRESQL query for the following: {query}")
+    print(response)
+
+    return str(response)
+
+
+def evalute(question, sql_query, result):
+    try:
+        openai_client = openai.OpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY"),
+        )
+        prompt = f"""
+            Given the following question:\n {question} \n\n
+
+            The generated SQL Query:\n {sql_query} \n\n
+
+            and the following result from the query:\n {result} \n\n
+
+            Respond with a proper sentence answering the question
+        """
+
+        response = openai_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": 
+                 """
+                    Given the following question:
+                    What is the total value of all the sales?
+                    
+
+                    The generated SQL Query:
+                    SELECT SUM(amount) AS total_sales_value FROM sale_advance_payment_inv
+                    
+
+                    and the following result from the query:
+                    [(32,)]
+                    
+
+                    Response:
+                    There were $320 dollars of sales
+                 """},
+            ],
+            temperature=0.5,
+        )
+
+        gpt_output = response.choices[0].message.content
+
+        return str(gpt_output)
+    except Exception as e:
+        print(f"Error generating caption: {e}")
+        return None
+
+
+def straight_answer(question):
+    try:
+        openai_client = openai.OpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY"),
+        )
+
+        response = openai_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": question},
+                {"role": "user", "content": ""},
+            ],
+            temperature=0.5,
+        )
+
+        gpt_output = response.choices[0].message.content
+
+        return str(gpt_output)
+    except Exception as e:
+        print(f"Error generating caption: {e}")
+        return None
+
+
+def answer(question):
+    try:
+        sql_query = create_sql_query(question)
+
+        if sql_query != 'None':
+            result, stat = perform_sql_query(sql_query)
+
+            if stat:
+                print(result)
+                final_response = evalute(question, sql_query, result)
+
+                if final_response:
+                    print(final_response)
+
+            else:
+                print("SQL Query failed")
+        
+        else:
+            print("No need for query")
+            resp = straight_answer(question)
+            print(resp)
+    
+    except Exception as e:
+        print("error: ", e)
+        resp = straight_answer(question)
+        print(resp)
+
+
+answer("How many tables are there in the database total?")
